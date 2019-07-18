86013fb729eaf7f4ae58464f2cc34903257cceb1
co5ta <costamonzili@gmail.com>
2019-07-18 12:16:15 +0200

    Add MVC pattern

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -33,13 +33,6 @@ class ViewController: UIViewController {
         return textView.text.firstIndex(of: "=") != nil
     }
     
-    // View Life cycles
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-    
-    
     // View actions
     @IBAction func tappedNumberButton(_ sender: UIButton) {
         guard let numberText = sender.title(for: .normal) else {
---
+++ CountOnMe/Model/Calculator.swift
@@ -0,0 +1,13 @@
+//
+//  Calculator.swift
+//  CountOnMe
+//
+//  Created by co5ta on 18/07/2019.
+//  Copyright © 2019 Vincent Saluzzo. All rights reserved.
+//
+
+import Foundation
+
+struct Calculator {
+    
+}
