6159771dfb1673ed6c6219b4c83864d51422e49c
co5ta <costamonzili@gmail.com>
2019-07-18 22:46:31 +0200

    Move calculation in Model

--- CountOnMe/Controller/ViewController.swift
+++ CountOnMe/Controller/ViewController.swift
@@ -51,29 +51,8 @@ class ViewController: UIViewController {
             return presentAlert(title: "Zero!", message: "Start a new calculation")
         }
         
-        // Create local copy of operations
-        var operationsToReduce = calculator.elements
-        
-        // Iterate over operations while an operand still here
-        while operationsToReduce.count > 1 {
-            let left = Int(operationsToReduce[0])!
-            let operand = operationsToReduce[1]
-            let right = Int(operationsToReduce[2])!
-            
-            let result: Int
-            switch operand {
-            case "+": result = left + right
-            case "-": result = left - right
-            case "x": result = left * right
-            case "÷": result = left / right
-            default: fatalError("Unknown operator !")
-            }
-            
-            operationsToReduce = Array(operationsToReduce.dropFirst(3))
-            operationsToReduce.insert("\(result)", at: 0)
-        }
-        
-        expression.append(" = \(operationsToReduce.first!)")
+        let result = calculator.getResult()
+        expression.append(" = \(result)")
     }
 
     private func presentAlert(title: String, message: String) {
--- CountOnMe/Model/Calculator.swift
+++ CountOnMe/Model/Calculator.swift
@@ -32,3 +32,30 @@ struct Calculator {
         return expression.firstIndex(of: "=") != nil
     }
 }
+
+extension Calculator {
+    func getResult() -> String {
+        // Create local copy of operations
+        var operationsToReduce = elements
+        
+        // Iterate over operations while an operand still here
+        while operationsToReduce.count > 1 {
+            let left = Int(operationsToReduce[0])!
+            let operand = operationsToReduce[1]
+            let right = Int(operationsToReduce[2])!
+            
+            let result: Int
+            switch operand {
+            case "+": result = left + right
+            case "-": result = left - right
+            case "x": result = left * right
+            case "÷": result = left / right
+            default: fatalError("Unknown operator !")
+            }
+            
+            operationsToReduce = Array(operationsToReduce.dropFirst(3))
+            operationsToReduce.insert("\(result)", at: 0)
+        }
+        return operationsToReduce[0]
+    }
+}
